a804cb7f73977374d98cc20973d16bb2f4a3ad70
Kannika Chaurung <kannika_chau@truecorp.co.th>
2017-08-11 14:34:27 +0700

    change delete callback func to async/await func.

--- app/controllers/demo_delete.js
+++ app/controllers/demo_delete.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const demo_delete = require('./../models/demo_delete');
 const demo_delete2 = require('./../models/demo_delete_asyncawait');
 
-router.delete('/deleteData', function(req, res) {
+router.delete('/deleteData', async function(req, res) {
   if (!req.query.lang) {
     req.query.lang = 'th';
   }
@@ -17,23 +17,11 @@ router.delete('/deleteData', function(req, res) {
     });
   }
 
-  demo_delete2.delContent(req.query.lang, req.query.id, function(err, result) {
-    if (err) {
-      return res.status(err.code).send({
-        code: err.code,
-        message: err.message
-      });
-    } else {
-      return res.status(200).send({
-        code: 200,
-        message: 'ID:'+req.query.id+' has been successfully deleted.'
-      });
-    }
+  await demo_delete2.delContent(req.query.lang, req.query.id);
+  return res.status(200).send({
+    code: 200,
+    message: 'ID:'+req.query.id+' has been successfully deleted.'
   });
 });
-
-
-
-
 
 module.exports = router;
--- app/models/demo_delete_asyncawait.js
+++ app/models/demo_delete_asyncawait.js
@@ -2,9 +2,26 @@ const request = require('request');
 
 const client = require('./../../config/db');
 
-exports.delContent = async function(lang, id, response) {
-    const query = "DELETE FROM contents2 WHERE lang = ? AND id = ?";
+// exports.delContent = function(lang, id) {
+//   const query = "DELETE FROM contents2 WHERE lang = ? AND id = ?";
+//   return new Promise(function (resolve, reject) {
+//     client.execute(query, [lang, id], {prepare: true}, function(err, result){
+//       if(err){
+//         reject(err);
+//       }else{
+//         resolve(result);
+//       }
+//     });
+//   });
+// }
+
+exports.delContent = async function(lang, id){
+  const query = "DELETE FROM contents2 WHERE lang = ? AND id = ?";
+  try {
     var result = await client.execute(query, [lang, id], {prepare: true});
-    response(null, result);
+    return result;
+  } catch (e) {
+    console.log(e);
+  }
 
 }
